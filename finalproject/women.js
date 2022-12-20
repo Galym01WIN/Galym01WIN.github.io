0557921ef73a261b4debbe61956016c562c917d4
Galym01WIN <210103298@stu.sdu.edu.kz>
2022-12-20 15:15:27 +0600

    sdgfsd

--- finalproject/women.js
+++ finalproject/women.js
@@ -31,48 +31,23 @@ for(var i = 0 ; i < link.length; i++){
 ///fghgfhgfhgfhgfhfgh
 ///f/ghfghgf/h+gfh/+gh/gfh/gf/h/h/gf/hf 
 
-// const link = document.querySelectorAll('.link')
-// const hoverReveal = document.querySelectorAll('.hover-reveal')
-// const linkImages = document.querySelectorAll('.hidden-img')
+const link = document.querySelectorAll('.link')
+const hoverReveal = document.querySelectorAll('.hover-reveal')
+const linkImages = document.querySelectorAll('.hidden-img')
 
-// for(var i = 0 ; i < link.length; i++){
-//   link[i].addEventListener('.mousemove',(e)=> {
-//   hoverReveal[i].style.opacity = 1;
-//   hoverReveal[i].style.transform = 'translate(-100%,-50%) rotate (5deg)';
-//   linkImages[i].style.transform = 'scale(1,1)';
-//   hoverReveal[i].style.left = e.clientX + "px";
-//   })
+for(var i = 0 ; i < link.length; i++){
+  link[i].addEventListener('.mousemove',(e)=> {
+  hoverReveal[i].style.opacity = 1;
+  hoverReveal[i].style.transform = 'translate(-100%,-50%) rotate (5deg)';
+  linkImages[i].style.transform = 'scale(1,1)';
+  hoverReveal[i].style.left = e.clientX + "px";
+  })
 
-//   link[i].addEventListener('.mouseleave',(e)=>{
-//     hoverReveal[i].style.opacity = 0;
-//     hoverReveal[i].style.transform = 'translate(-50%,-50%) rotate (-5deg)';
-//     linkImages[i].style.transform = 'scale(0.8,0.8)';
-//     hoverReveal[i].style.left = e.clientX + "px";
-
-
-//   })
-// }
-
-
-
-
-// const link = document.querySelectorAll('.link')
-// const hoverReveal = document.querySelectorAll('.hover-reveal')
-// const linkImages = document.querySelectorAll('.hidden-img')
-
-// for(var i = 0 ; i < link.length; i++){
-//   link[i].addEventListener('.mousemove',(e)=> {
-//   hoverReveal[i].style.opacity = 1;
-//   hoverReveal[i].style.transform = 'translate(-100%,-50%) rotate (5deg)';
-//   linkImages[i].style.transform = 'scale(1,1)';
-//   hoverReveal[i].style.left = e.clientX + "px";
-//   })
-
-//   link[i].addEventListener('.mouseleave',(e)=>{
-//     hoverReveal[i].style.opacity = 0;
-//     hoverReveal[i].style.transform = 'translate(-50%,-50%) rotate (-5deg)';
-//     linkImages[i].style.transform = 'scale(0.8,0.8)';
-//     hoverReveal[i].style.left = e.clientX + "px";
+  link[i].addEventListener('.mouseleave',(e)=>{
+    hoverReveal[i].style.opacity = 0;
+    hoverReveal[i].style.transform = 'translate(-50%,-50%) rotate (-5deg)';
+    linkImages[i].style.transform = 'scale(0.8,0.8)';
+    hoverReveal[i].style.left = e.clientX + "px";
 
 
 //   })
@@ -81,24 +56,49 @@ for(var i = 0 ; i < link.length; i++){
 
 
 
+const link = document.querySelectorAll('.link')
+const hoverReveal = document.querySelectorAll('.hover-reveal')
+const linkImages = document.querySelectorAll('.hidden-img')
 
-// const link = document.querySelectorAll('.link')
-// const hoverReveal = document.querySelectorAll('.hover-reveal')
-// const linkImages = document.querySelectorAll('.hidden-img')
+for(var i = 0 ; i < link.length; i++){
+  link[i].addEventListener('.mousemove',(e)=> {
+  hoverReveal[i].style.opacity = 1;
+  hoverReveal[i].style.transform = 'translate(-100%,-50%) rotate (5deg)';
+  linkImages[i].style.transform = 'scale(1,1)';
+  hoverReveal[i].style.left = e.clientX + "px";
+  })
 
-// for(var i = 0 ; i < link.length; i++){
-//   link[i].addEventListener('.mousemove',(e)=> {
-//   hoverReveal[i].style.opacity = 1;
-//   hoverReveal[i].style.transform = 'translate(-100%,-50%) rotate (5deg)';
-//   linkImages[i].style.transform = 'scale(1,1)';
-//   hoverReveal[i].style.left = e.clientX + "px";
+  link[i].addEventListener('.mouseleave',(e)=>{
+    hoverReveal[i].style.opacity = 0;
+    hoverReveal[i].style.transform = 'translate(-50%,-50%) rotate (-5deg)';
+    linkImages[i].style.transform = 'scale(0.8,0.8)';
+    hoverReveal[i].style.left = e.clientX + "px";
+
+
 //   })
+// }
 
-//   link[i].addEventListener('.mouseleave',(e)=>{
-//     hoverReveal[i].style.opacity = 0;
-//     hoverReveal[i].style.transform = 'translate(-50%,-50%) rotate (-5deg)';
-//     linkImages[i].style.transform = 'scale(0.8,0.8)';
-//     hoverReveal[i].style.left = e.clientX + "px";
+
+
+
+
+const link = document.querySelectorAll('.link')
+const hoverReveal = document.querySelectorAll('.hover-reveal')
+const linkImages = document.querySelectorAll('.hidden-img')
+
+for(var i = 0 ; i < link.length; i++){
+  link[i].addEventListener('.mousemove',(e)=> {
+  hoverReveal[i].style.opacity = 1;
+  hoverReveal[i].style.transform = 'translate(-100%,-50%) rotate (5deg)';
+  linkImages[i].style.transform = 'scale(1,1)';
+  hoverReveal[i].style.left = e.clientX + "px";
+  })
+
+  link[i].addEventListener('.mouseleave',(e)=>{
+    hoverReveal[i].style.opacity = 0;
+    hoverReveal[i].style.transform = 'translate(-50%,-50%) rotate (-5deg)';
+    linkImages[i].style.transform = 'scale(0.8,0.8)';
+    hoverReveal[i].style.left = e.clientX + "px";
 
 
 //   })
